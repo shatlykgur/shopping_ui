aee12abc179a22057f81fe03f756eb5e58336c0e
Shatlyk Gurbandurdyyev <shatlykgur@gmail.com>
2021-01-26 22:55:09 +0300

    kategori resim

--- lib/kategori.dart
+++ lib/kategori.dart
@@ -10,6 +10,72 @@ class Kategori extends StatefulWidget {
 }
 
 class _KategoriState extends State<Kategori> {
+  List<Widget> gosterilecekListe;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.kategori == "temel gıda") {
+      gosterilecekListe = [
+        urunKarti(),
+        Container(color: Colors.blue),
+        Container(color: Colors.green),
+        Container(color: Colors.teal),
+      ];
+    } else if (widget.kategori == "şekerleme") {
+      gosterilecekListe = [
+        Container(color: Colors.red),
+        Container(color: Colors.blue),
+      ];
+    } else if (widget.kategori == "içecekler") {
+      gosterilecekListe = [
+        Container(color: Colors.teal),
+        Container(color: Colors.red),
+        Container(color: Colors.blue),
+        Container(color: Colors.green),
+      ];
+    } else if (widget.kategori == "temizlik") {
+      gosterilecekListe = [
+        Container(color: Colors.blue),
+        Container(color: Colors.green),
+        Container(color: Colors.teal),
+      ];
+    }
+  }
+
+  Widget urunKarti() {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              blurRadius: 4.0,
+              spreadRadius: 2.0,
+            )
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 120.0,
+            height: 80.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    "https://cdn.pixabay.com/photo/2015/06/26/15/31/oil-822618_960_720.jpg"),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GridView.count(
@@ -18,12 +84,7 @@ class _KategoriState extends State<Kategori> {
       crossAxisSpacing: 12.0,
       padding: EdgeInsets.all(10.0),
       childAspectRatio: 1,
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.blue),
-        Container(color: Colors.green),
-        Container(color: Colors.teal),
-      ],
+      children: gosterilecekListe,
     );
   }
 }
